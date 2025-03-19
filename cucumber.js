0b9d7778bc4aca7f59e816fe3b122f12d03f21e6
sayf <sayfkd30@gmail.com>
2025-03-19 10:25:14 +0100

    init projet

--- cucumber.js
+++ cucumber.js
@@ -1,22 +1,21 @@
-module.exports = {       
-   reporter: "allure-playwright",
-    default: {
-      require: [
-        "step-definitions/**/*.ts",   // Path to step definitions
-        "hooks/**/*.ts"               // Path to hooks
-      ],
-      format: [
-        "allure-cucumberjs/reporter",
-       "json:reports/cucumber-report.json", // JSON report for analysis
-       "html:reports/cucumber-report.html"  // HTML report for human-readable output
+module.exports = {
+  default: {
+    require: [
+      "step-definitions/**/*.ts",   // Path to step definitions
+      "hooks/**/*.ts"               // Path to hooks
+    ],
+    format: [
+      "allure-cucumberjs",                   // Pour les rapports Allure
 
-      ],
-      //tags: "@valid or @invalid",  // Run specific tests with tags
-      worldParameters: {           // Custom parameters for tests
-        baseUrl: "https://opensource-demo.orangehrmlive.com"
-      },
-      requireModule: ["ts-node/register"], // Enable TypeScript
-      timeout: 10000, // Set test timeout (10s)
+      "progress",                           // Affiche la progression dans le terminal
+      "json:reports/cucumber-report.json",   // JSON report pour Allure
+      "html:reports/cucumber-report.html"    // HTML report pour la lecture humaine
+    ],
+    tags: "@valid or @invalid",              // Filtrage des tests par tag
+    worldParameters: {                       
+      baseUrl: "https://opensource-demo.orangehrmlive.com"
     },
-  };
-  
+    requireModule: ["ts-node/register"],     // Activer TypeScript
+    timeout: 10000                           // Temps d'exécution max (10s)
+  },
+};
